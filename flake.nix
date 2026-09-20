19f0e8977cf32a1a1fb3a9740ae6fc422db9aa88
Philippe Laflamme <philippe@hopper.com>
2026-09-20 11:44:27 -0400

    fix: add `-debug` derivations instead of impure builds

--- flake.nix
+++ flake.nix
@@ -21,31 +21,27 @@
           ...
         }:
         let
-          isCI = (builtins.getEnv "CI") == "true";
           workspaceCargo = fromTOML (builtins.readFile ./Cargo.toml);
           keyroostCargo = fromTOML (builtins.readFile ./crates/keyroost/Cargo.toml);
           keyroostctlCargo = fromTOML (builtins.readFile ./crates/keyroostctl/Cargo.toml);
-          version = workspaceCargo.workspace.package.version;
-        in
-        {
-          packages = {
-            keyroost = pkgs.rustPlatform.buildRustPackage {
-              pname = keyroostCargo.package.name;
-              inherit version;
 
+          buildKeyroostPackage =
+            crateCargo: extraBuildInputs: debug:
+            pkgs.rustPlatform.buildRustPackage {
+              pname = crateCargo.package.name;
+              version = workspaceCargo.workspace.package.version;
               src = ./.;
-
               cargoLock = {
                 lockFile = ./Cargo.lock;
               };
 
               cargoBuildFlags = [
                 "-p"
-                "keyroost"
+                crateCargo.package.name
               ];
 
-              buildType = if isCI then "debug" else "release";
-              dontStrip = isCI;
+              buildType = if debug then "debug" else "release";
+              dontStrip = debug;
 
               buildNoDefaultFeatures = true;
 
@@ -53,40 +49,16 @@
                 pkg-config
               ];
 
-              buildInputs =
-                with pkgs;
-                [
-                  pcsclite
-                ]
-                ++ pkgs.lib.optionals pkgs.stdenv.hostPlatform.isLinux [
-                  libGL
-                  libx11
-                  libxcb
-                  libxcursor
-                  libxi
-                  libxkbcommon
-                  libxrandr
-                  wayland
-                ];
+              buildInputs = [ pkgs.pcsclite ] ++ extraBuildInputs;
 
-              postFixup = pkgs.lib.optionalString pkgs.stdenv.hostPlatform.isLinux ''
-                patchelf --set-rpath "${
-                  pkgs.lib.makeLibraryPath [
-                    pkgs.libGL
-                    pkgs.libx11
-                    pkgs.libxcb
-                    pkgs.libxcursor
-                    pkgs.libxi
-                    pkgs.libxkbcommon
-                    pkgs.libxrandr
-                    pkgs.pcsclite
-                    pkgs.wayland
-                  ]
-                }" $out/bin/keyroost
+              postFixup = pkgs.lib.optionalString (pkgs.stdenv.hostPlatform.isLinux) ''
+                patchelf \
+                  --set-rpath "${pkgs.lib.makeLibraryPath ([ pkgs.pcsclite ] ++ extraBuildInputs)}" \
+                  $out/bin/${crateCargo.package.name}
               '';
 
               meta = {
-                description = keyroostCargo.package.description;
+                description = crateCargo.package.description;
                 homepage = "https://github.com/framefilter/keyroost";
                 license = with pkgs.lib.licenses; [
                   mit
@@ -96,46 +68,29 @@
               };
             };
 
-            keyroostctl = pkgs.rustPlatform.buildRustPackage {
-              pname = keyroostctlCargo.package.name;
-              inherit version;
+          keyroostExtraBuildInputs = pkgs.lib.optionals pkgs.stdenv.hostPlatform.isLinux (
+            with pkgs;
+            [
+              libGL
+              libx11
+              libxcb
+              libxcursor
+              libxi
+              libxkbcommon
+              libxrandr
+              wayland
+            ]
+          );
 
-              src = ./.;
-
-              cargoLock = {
-                lockFile = ./Cargo.lock;
-              };
-
-              cargoBuildFlags = [
-                "-p"
-                "keyroostctl"
-              ];
-
-              buildType = if isCI then "debug" else "release";
-              dontStrip = isCI;
-
-              nativeBuildInputs = with pkgs; [
-                pkg-config
-              ];
-
-              buildInputs = with pkgs; [
-                pcsclite
-              ];
-
-              postFixup = pkgs.lib.optionalString pkgs.stdenv.hostPlatform.isLinux ''
-                patchelf --set-rpath "${pkgs.lib.makeLibraryPath [ pkgs.pcsclite ]}" $out/bin/keyroostctl
-              '';
-
-              meta = {
-                description = keyroostctlCargo.package.description;
-                homepage = "https://github.com/framefilter/keyroost";
-                license = with pkgs.lib.licenses; [
-                  mit
-                  asl20
-                ];
-                platforms = pkgs.lib.platforms.unix;
-              };
-            };
+          buildKeyroost = buildKeyroostPackage keyroostCargo keyroostExtraBuildInputs;
+          buildKeyroostctl = buildKeyroostPackage keyroostctlCargo [ ];
+        in
+        {
+          packages = {
+            keyroost = buildKeyroost false;
+            keyroost-debug = buildKeyroost true;
+            keyroostctl = buildKeyroostctl false;
+            keyroostctl-debug = buildKeyroostctl true;
 
             default = config.packages.keyroost;
           };
